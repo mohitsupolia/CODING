71bd21a169a6d7d664360fc6185a85d79b378cf3
mohitsupolia <104218088+mohitsupolia@users.noreply.github.com>
2022-05-29 01:48:19 +0530

    Create Primenumber.java

---
+++ Primenumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main
+{
+void prime(int n)
+{
+    int i,j;
+    for(i=2;i<=n;i++)
+    {
+        for(j=2;j<=i;j++)
+        {
+           if(i%j==0)
+            break;
+        }
+        if(i==j)
+        System.out.println("prime number="+j);
+    }
+    System.out.println("\n");
+}
+public static void main(String mohit[])
+{
+    int n;
+    System.out.println("enter the number:");
+    Scanner sc=new Scanner(System.in);
+    n=sc.nextInt();
+    Main m=new Main();
+    m.prime(n);
+}
+}
